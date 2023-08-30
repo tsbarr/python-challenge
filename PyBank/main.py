b58d54b61dde7638902cc54a5a010e6f3a173e0e
tsbarr <63961884+tsbarr@users.noreply.github.com>
2023-08-30 15:46:56 -0400

    PyBank: fix change calculation (prevPL - thisPL)

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,9 +9,12 @@ import csv
 
 # --- for storing output
 totalMonths = 0  # count months
-totalChange = 0  # sum all changes
-minChange = ["", 0]  # lowest change and its date [date, change]
-maxChange = ["", 0]  # highest change and its date [date, change]
+totalPL = 0  # sum all profit/losses
+totalChange = 0  # calculate difference from prev PL for each and sum all, to calculate average later
+# max decrease = min Change in PL and its date [date, decrease]
+minChange = ["", 0]
+# max increase = max Change in PL and its date [date, increase]
+maxChange = ["", 0]
 
 # --- input source path
 csvpath = os.path.join('resources', 'budget_data.csv')
@@ -25,34 +28,52 @@ with open(csvpath) as csvfile:
     # get headers
     headers = next(csvreader)
 
+    # get first row before itaring
+    # because we are not calculating change for it
+    # but we need it to calculate the first change
+    firstRow = next(csvreader)
+    # count first month and profit/losses
+    totalMonths = 1
+    totalPL = int(firstRow[1])  # cast str to int
+    # set previousPL to profit/losses of first month
+    previousPL = int(firstRow[1])  # cast str to int
+
     # iterate through rest of rows
     for row in csvreader:
         # get data from this row
         thisDate = row[0]
-        thisChange = int(row[1])  # cast str to int
+        thisPL = int(row[1])  # cast str to int
 
         # increase totalMonths by 1
         totalMonths += 1
+        # increase totalPL by thisPL
+        totalPL += thisPL
 
-        # increase totalchange by thisChange
+        # calculate thisChange
+        thisChange = thisPL - previousPL
+        # increase totalChange by thisChange
         totalChange += thisChange
 
         # check if thisChange is lower than minChange
         if thisChange < minChange[1]:
-            # if so, update minChange with current row
+            # if so, update minChange with data from this row
             minChange = [thisDate, thisChange]
-
         # check if thisChange is higher than minChange
         if thisChange > maxChange[1]:
-            # if so, update maxChange with current row
+            # if so, update maxChange with data from this row
             maxChange = [thisDate, thisChange]
-# ------- close file stream --------
+        # set previousPL as thisPL before looping back to next row
+        previousPL = thisPL
 
-# average change is totalChange / totalMonths
-averageChange = totalChange / totalMonths
+# ------- close file stream --------
+print("\n\n")
+# average change is totalChange divided by totalMonths-1
+# (because no change for first month)
+# round to 2 decimal places
+averageChange = round(totalChange / (totalMonths - 1), 2)
 
 # we now have all necessary values, so we format the output in a string
-outputS = f"Financial Analysis\n\n------------------------\n\nTotal Months: {totalMonths}\n\nTotal: {totalChange}\n\nAverange Change: {averageChange}\n\nGreatest Increase in Profits: {maxChange[0]} ({maxChange[1]})\n\nGreatest Decrease in Profits: {minChange[0]} ({minChange[1]})"
+outputS = f"Financial Analysis\n\n------------------------\n\nTotal Months: {totalMonths}\n\nTotal: {totalPL}\n\nAverange Change: {averageChange}\n\nGreatest Increase in Profits: {maxChange[0]} ({maxChange[1]})\n\nGreatest Decrease in Profits: {minChange[0]} ({minChange[1]})"
 
 # write output to file
 
