330f0a6d03bd0537617023d0462fdb71a3c5a492
tsbarr <63961884+tsbarr@users.noreply.github.com>
2023-08-31 13:21:24 -0400

    PyPoll: no nested dict

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,73 +8,76 @@ import os
 import csv
 
 # --- variables that will store output
-# a nested dictionary with key = candidate, value = dict{voteCount, percentOfVote}
-# https://www.w3schools.com/python/python_dictionaries_nested.asp
-candidateInfo = {}
+# a dictionary with key = candidate, value = voteCount
+votesByCandidate = {}
 # sum of vote counts
 totalVotes = 0
 # string to concatenate the portion of the output for individual candidates
-outIndividualCandidates = ""
+outCandidateInfo = ""
 
 # --- read input
 # input source path
-csvpath = os.path.join('resources', 'election_data.csv')
+ipath = os.path.join('resources', 'election_data.csv')
 
 # ------- open file stream --------
 # open input csv file using path
-with open(csvpath) as csvfile:
+with open(ipath) as ifile:
     # csv reader, columns: id, county, candidate
-    csvreader = csv.reader(csvfile, delimiter=',')
+    ireader = csv.reader(ifile, delimiter=',')
 
     # skip headers
-    next(csvreader)
+    next(ireader)
 
     # iterate through rest of rows
-    for row in csvreader:
+    for row in ireader:
         # get name of candidate in the row
         thisCandidate = row[2]
-        # if thisCandidate exists in candidateInfo
+        # if thisCandidate exists in votesByCandidate
         # https://flexiple.com/python/check-if-key-exists-in-dictionary-python/
-        if thisCandidate in candidateInfo:
+        if thisCandidate in votesByCandidate:
             # increase thisCandidate voteCount by 1
-            candidateInfo[thisCandidate]["voteCount"] += 1
+            votesByCandidate[thisCandidate] += 1
         else:
-            # add it and set voteCount to 1
+            # add it and set to 1
             # https://www.w3schools.com/python/python_dictionaries_add.asp
-            candidateInfo[thisCandidate] = {"voteCount": 1}
+            votesByCandidate[thisCandidate] = 1
 # ------- close file stream --------
 
 # use max() with key argument to find winner
-# parameter iterable: candidate names (the keys of the dict candidateInfo)
-# key argument: lambda function that returns the candidate 'votecount'
+# parameter iterable: candidate names (the keys of the dict votesByCandidate)
+# key argument: lambda function that returns the candidate 'voteCount'
 # therefore, the voteCount will be used to sort and it will return the name with the highest voteCount
 # max function documentation: https://thepythonguru.com/python-builtin-functions/max/
 # how to lambda functions: https://www.w3schools.com/python/python_lambda.asp
-winner = max(
-    candidateInfo.keys(),
-    key=lambda thisCandidate: candidateInfo[thisCandidate]["voteCount"]
-)
+winner = max(votesByCandidate.keys(), key=lambda name: votesByCandidate[name])
 
 # --- totalVotes
-# iterate through candidates in candidateInfo
-for thisCandidate in candidateInfo:
+# iterate through candidates in votesByCandidate
+for thisCandidate in votesByCandidate:
     # add the voteCount of each candidate to totalVotes
-    totalVotes += candidateInfo[thisCandidate]["voteCount"]
+    totalVotes += votesByCandidate[thisCandidate]
 
 
-# --- percentOfVote
+# --- percentOfVote and output for individual candidates
 # now that we have the totalVotes, we can calculate percentOfVote
-# and format the candidate-level output, concatenating them in outIndividualCandidates
-for thisCandidate in candidateInfo:
-    # add percentOfVote to candidate entry,
-    # as voteCount / totalVotes
-    candidateInfo[thisCandidate]["percentOfVotes"] = candidateInfo[thisCandidate]["voteCount"] / totalVotes
-    # format and add thisCandidate info to output string outIndividualCandidates
-    outIndividualCandidates = outIndividualCandidates + \
-        f"{thisCandidate}: {round(candidateInfo[thisCandidate]['percentOfVotes']*100, 3)}% ({candidateInfo[thisCandidate]['voteCount']})\n\n"
+# and format the candidate-level output, concatenating them in outCandidateInfo
+for thisCandidate in votesByCandidate:
+    # percentOfVote = voteCount / totalVotes, not necessary to store long-term
+    percentOfVotes = votesByCandidate[thisCandidate] / totalVotes
+    # format and add thisCandidate info to output string
+    outCandidateInfo += f"{thisCandidate}: {round(percentOfVotes*100, 3)}% ({votesByCandidate[thisCandidate]})\n\n"
 
-# --- format the whole output, store it in a string
-outString = f"Election Results\n\n-------------------------\n\nTotal Votes: {totalVotes}\n\n-------------------------\n\n{outIndividualCandidates}-------------------------\n\nWinner: {winner}\n\n-------------------------"
+# --- format the whole output, store it in outString
+outString = f"\
+Election Results\n\n\
+-------------------------\n\n\
+Total Votes: {totalVotes}\n\n\
+-------------------------\n\n\
+{outCandidateInfo}\
+-------------------------\n\n\
+Winner: {winner}\n\n\
+-------------------------\
+"
 
 # # --- write output to file
 # # output source path
