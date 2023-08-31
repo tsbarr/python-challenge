24b685b4896162c28a5d66b2a06aefb43de09b52
tsbarr <63961884+tsbarr@users.noreply.github.com>
2023-08-31 14:17:13 -0400

    PyPoll: use Counter, dict subclass, to count vote

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,17 +3,12 @@
 # Script Author: Tania Barrera (tsbarr)
 # -----------------------------------------
 
-# import modules
+# --- import modules
 import os
 import csv
-
-# --- variables that will store output
-# a dictionary with key = candidate, value = voteCount
-votesByCandidate = {}
-# sum of vote counts
-totalVotes = 0
-# string to concatenate the portion of the output for individual candidates
-outCandidateInfo = ""
+# used to count votes
+# documentation: https://docs.python.org/3/library/collections.html#collections.Counter
+from collections import Counter
 
 # --- read input
 # input source path
@@ -25,22 +20,15 @@ with open(ipath) as ifile:
     # csv reader, columns: id, county, candidate
     ireader = csv.reader(ifile, delimiter=',')
 
-    # skip headers
-    next(ireader)
+    # store header row
+    header = next(ireader)
 
-    # iterate through rest of rows
-    for row in ireader:
-        # get name of candidate in the row
-        thisCandidate = row[2]
-        # if thisCandidate exists in votesByCandidate
-        # https://flexiple.com/python/check-if-key-exists-in-dictionary-python/
-        if thisCandidate in votesByCandidate:
-            # increase thisCandidate voteCount by 1
-            votesByCandidate[thisCandidate] += 1
-        else:
-            # add it and set to 1
-            # https://www.w3schools.com/python/python_dictionaries_add.asp
-            votesByCandidate[thisCandidate] = 1
+    # using the rest of the rows in ireader,
+    # extract the column of candidate names into a tuple
+    # and use Counter() to count the instances of each name
+    # voteCounts is now a dict with keys = candidate names, values = vote count
+    # Based on "Counting Files by Type" example at: https://realpython.com/python-counter/
+    voteCounts = Counter((row[2] for row in ireader))
 # ------- close file stream --------
 
 # use max() with key argument to find winner
@@ -49,23 +37,26 @@ with open(ipath) as ifile:
 # therefore, the voteCount will be used to sort and it will return the name with the highest voteCount
 # max function documentation: https://thepythonguru.com/python-builtin-functions/max/
 # how to lambda functions: https://www.w3schools.com/python/python_lambda.asp
-winner = max(votesByCandidate.keys(), key=lambda name: votesByCandidate[name])
+winner = max(voteCounts.keys(), key=lambda name: voteCounts[name])
 
 # --- totalVotes
+# sum of values in voteCounts
+totalVotes = 0
 # iterate through candidates in votesByCandidate
-for thisCandidate in votesByCandidate:
+for thisCandidate in voteCounts:
     # add the voteCount of each candidate to totalVotes
-    totalVotes += votesByCandidate[thisCandidate]
+    totalVotes += voteCounts[thisCandidate]
 
 
 # --- percentOfVote and output for individual candidates
 # now that we have the totalVotes, we can calculate percentOfVote
 # and format the candidate-level output, concatenating them in outCandidateInfo
-for thisCandidate in votesByCandidate:
+outCandidateInfo = ""
+for thisCandidate in voteCounts:
     # percentOfVote = voteCount / totalVotes, not necessary to store long-term
-    percentOfVotes = votesByCandidate[thisCandidate] / totalVotes
+    percentOfVotes = voteCounts[thisCandidate] / totalVotes
     # format and add thisCandidate info to output string
-    outCandidateInfo += f"{thisCandidate}: {round(percentOfVotes*100, 3)}% ({votesByCandidate[thisCandidate]})\n\n"
+    outCandidateInfo += f"{thisCandidate}: {round(percentOfVotes*100, 3)}% ({voteCounts[thisCandidate]})\n\n"
 
 # --- format the whole output, store it in outString
 outString = f"\
